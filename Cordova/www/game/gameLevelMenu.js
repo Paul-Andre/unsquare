94183bf32b2702d9d682888eaed5736e7adf3c66
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2024-02-06 01:56:46 -0500

    2024_feb_06_01.json

--- Cordova/www/game/gameLevelMenu.js
+++ Cordova/www/game/gameLevelMenu.js
@@ -4,7 +4,7 @@ var gameLevelMenu = activateLevelMenu("gameLevelMenu", false);
 screenManager.additionalFunctions.gameLevelMenu = gameLevelMenu;
 
 
-let bookUrl = "2024_feb_05_01.json";
+let bookUrl = "2024_feb_06_01.json";
 
 //https://stackoverflow.com/a/35294675
 let request = new XMLHttpRequest();
