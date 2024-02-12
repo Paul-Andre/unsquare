9329f26de1bbbd2e33211f1b316dcb190c6f817a
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2024-02-12 14:35:46 -0500

    move level tracking to new analytics.js

--- Cordova/www/game/game2.js
+++ Cordova/www/game/game2.js
@@ -1,35 +1,6 @@
 "use strict";
 
 
-function getGtagLevelName(level, book) {
-  let a = "Level_" + (level.index+1) + " " + book.source + " " + level.getFullIdentifier() + " tag_v1";
-  console.log(a)
-  return a;
-}
-
-function trackLevelStart(level, book) {
-  // Wrapping in setTimeout to minimize issues if an error happens.
-  setTimeout(function () {
-    if (gtag) {
-      let name = getGtagLevelName(level, book);
-      gtag("event", "level_start", {
-        level_name: name,
-      });
-    }
-  }, 0);
-}
-function trackLevelEnd(level, book) {
-  // Wrapping in setTimeout to minimize issues if an error happens.
-  setTimeout(function () {
-    if (gtag) {
-      let name = getGtagLevelName(level, book);
-      gtag("event", "level_end", {
-        level_name: name,
-        success: true
-      });
-    }
-  }, 0);
-}
 
 /// This is what does the basics of drawing the tiles to the screen.
 ///
