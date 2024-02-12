29553a3b817e82a4797f4362545ad1068b8db62e
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2024-02-12 14:50:33 -0500

    Add glow to first icon

--- Cordova/www/game/activateLevelMenu.js
+++ Cordova/www/game/activateLevelMenu.js
@@ -107,7 +107,7 @@ function activateLevelMenu(elementId, isEditor) {
   };
 
   // TODO: rename; this creates an html node
-  levelMenu.createLevelInfo = function (level, state) {
+  levelMenu.createLevelInfo = function (level, state, glow) {
 
     let element = htmlStringToElement( `<div class="level_icon">
     <canvas class="level_icon_image"> </canvas>
@@ -146,6 +146,10 @@ function activateLevelMenu(elementId, isEditor) {
       }[state];
 
       element.classList.add(stateClass);
+      if (glow) {
+        element.classList.add("icon_glow");
+      }
+
     }
 
     return element;
@@ -207,8 +211,9 @@ function activateLevelMenu(elementId, isEditor) {
 
     for (var i = 0; i < this.book.levels.length; i++) {
       let level = this.book.levels[i];
+      let glow = (!isEditor && i==0 && states[i] == 2);
       // check par, and based on it figure out the restriction level.
-      this.container.appendChild(levelMenu.createLevelInfo(level, isEditor?2:states[i] ));
+      this.container.appendChild(levelMenu.createLevelInfo(level, isEditor?2:states[i], glow));
     }
 
   };
