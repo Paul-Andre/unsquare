48c106bcee969c4585703280626c9c7887e71e22
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-08-24 00:14:14 -0400

    refactor: Convert screenManager.js to modern ES6 class structure
    - Created ScreenManager class to encapsulate screen navigation logic
    - Moved loading screen initialization into constructor
    - Used arrow function for proper 'this' binding in onHide callback
    - Added JSDoc comments for better documentation
    - Used const instead of var for better scoping
    - Maintained backward compatibility with global screenManager instance

--- web/www/screenManager.js
+++ web/www/screenManager.js
@@ -2,20 +2,43 @@
 
 "use strict";
 
-var screenManager = {
-  additionalFunctions: {},
-  stack: [],
-  currentScreenName: "home",
-  currentScreen: document.getElementById("loading"),
-  executeFunction: function (screenName, funcName) {
+class ScreenManager {
+  constructor() {
+    this.additionalFunctions = {};
+    this.stack = [];
+    this.currentScreenName = "home";
+    this.currentScreen = document.getElementById("loading");
+    
+    // Initialize loading screen functions
+    this.additionalFunctions.loadingScreen = {
+      onHide: () => {
+        document.getElementById("loading").remove();
+        this.stack.shift(); // This is to remove the loadingScreen from the stack.
+        delete this.additionalFunctions.loadingScreen;
+      },
+    };
+  }
+
+  /**
+   * Executes a function for a specific screen if it exists
+   * @param {string} screenName - The name of the screen
+   * @param {string} funcName - The name of the function to execute
+   */
+  executeFunction(screenName, funcName) {
     if (
       screenName in this.additionalFunctions &&
       funcName in this.additionalFunctions[screenName]
     ) {
       this.additionalFunctions[screenName][funcName]();
     }
-  },
-  switchTo: function (screenName, keepAsIs) {
+  }
+
+  /**
+   * Switches to a new screen
+   * @param {string} screenName - The name of the screen to switch to
+   * @param {boolean} keepAsIs - Whether to keep the current screen state
+   */
+  switchTo(screenName, keepAsIs) {
     if (this.currentScreen) {
       this.currentScreen.classList.remove("variant_shown");
     } else {
@@ -26,6 +49,7 @@ var screenManager = {
       name: this.currentScreenName,
       keepAsIs: !!keepAsIs,
     });
+    
     if (!keepAsIs) {
       this.executeFunction(this.currentScreenName, "onHide");
     }
@@ -34,25 +58,25 @@ var screenManager = {
     this.currentScreen = document.getElementById(screenName);
     this.currentScreen.classList.add("variant_shown");
     this.executeFunction(this.currentScreenName, "onShow");
-  },
-  goBack: function () {
+  }
+
+  /**
+   * Goes back to the previous screen
+   */
+  goBack() {
     this.currentScreen.classList.remove("variant_shown");
-    var popped = this.stack.pop();
+    const popped = this.stack.pop();
     this.executeFunction(this.currentScreenName, "onHide");
 
     this.currentScreenName = popped.name;
     this.currentScreen = document.getElementById(this.currentScreenName);
     this.currentScreen.classList.add("variant_shown");
+    
     if (!popped.keepAsIs) {
       this.executeFunction(this.currentScreenName, "onShow");
     }
-  },
-};
+  }
+}
 
-screenManager.additionalFunctions.loadingScreen = {
-  onHide: function () {
-    document.getElementById("loading").remove();
-    screenManager.stack.shift(); // This is to remove the loadingScreen from the stack.
-    delete screenManager.additionalFunctions.loadingScreen;
-  },
-};
+// Create global instance for backward compatibility
+const screenManager = new ScreenManager();
