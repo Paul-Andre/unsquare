4132b27d7a32ddcb0895ef59de6903e3ec4c96c7
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-08-24 00:43:30 -0400

    refactor: Convert colorSchemes.js to modern ES6 class hierarchy
    - Created base ColorScheme class with abstract unsquare/resquare methods
    - Created specific color scheme classes: BWColorScheme, TriColorScheme, RainbowColorScheme, Rainbow2ColorScheme
    - Each class extends ColorScheme and implements its own unsquare/resquare logic
    - Maintained the same colorSchemes object structure for compatibility
    - Improved code organization with proper inheritance and encapsulation

--- web/www/colorSchemes.js
+++ web/www/colorSchemes.js
@@ -1,183 +1,146 @@
 "use strict";
 
-var colorSchemes = {
-  BW: {
-    cells: {
-      1: {
-        fill: "white",
-      },
-      2: {
-        fill: "#333333",
-      },
-    },
+class ColorScheme {
+  constructor(name, cells, arithmetic) {
+    this.name = name;
+    this.cells = cells;
+    this.arithmetic = arithmetic;
+  }
 
-    unsquare: function (e) {
-      return e == 1 ? 2 : e == 2 ? 1 : 0;
-    },
+  unsquare(e) {
+    throw new Error("unsquare method must be implemented by subclass");
+  }
 
-    resquare: function (e) {
-      return e == 1 ? 2 : e == 2 ? 1 : 0;
-    },
-    // this is to transition to the new level format.
-    arithmetic: {
-      type: "modular",
-      modulus: 2,
-    },
-  },
-
-  tri: {
-    cells: {
-      1: {
-        fill: "#F9FAC8",
-      },
-      2: {
-        fill: "#0431D4",
-      },
-      3: {
-        fill: "#FF0040",
-      },
-    },
-
-    unsquare: function (e) {
-      return e == 1 ? 3 : e == 3 ? 2 : e == 2 ? 1 : 0;
-    },
-
-    resquare: function (e) {
-      return e == 3 ? 1 : e == 2 ? 3 : e == 1 ? 2 : 0;
-    },
-
-    arithmetic: {
-      type: "modular",
-      modulus: 3,
-    },
-  },
-
-  rainbow: {
-    cells: {
-      1: {
-        fill: "#BE81F7",
-      },
-
-      2: {
-        fill: "#5882FA",
-      },
-
-      3: {
-        fill: "#58FAF4",
-      },
-
-      4: {
-        fill: "#82FA58",
-      },
-
-      5: {
-        fill: "#F4FA58",
-      },
-
-      6: {
-        fill: "#FE9A2E",
-      },
-
-      7: {
-        fill: "#FA5858",
-      },
-    },
-
-    unsquare: function (e) {
-      e++;
-      if (e == 8) {
-        e = 1;
-      }
-      return e;
-    },
-
-    resquare: function (e) {
-      e--;
-      if (e == 0) {
-        e = 7;
-      }
-      return e;
-    },
-
-    arithmetic: {
-      type: "",
-      modulus: 7,
-    },
-  },
-
-  rainbow2: {
-    cells: {
-      1: {
-        fill: "white",
-      },
-
-      2: {
-        fill: "#BE81F7",
-      },
-
-      3: {
-        fill: "#5882FA",
-      },
-
-      4: {
-        fill: "#58FAF4",
-      },
-
-      5: {
-        fill: "#82FA58",
-      },
-
-      6: {
-        fill: "#F4FA58",
-      },
-
-      7: {
-        fill: "#FE9A2E",
-      },
-
-      8: {
-        fill: "#FA5858",
-      },
-    },
-
-    unsquare: function (e) {
-      e++;
-      if (e == 9) {
-        e = 1;
-      }
-      return e;
-    },
-
-    resquare: function (e) {
-      e--;
-      if (e == 0) {
-        e = 8;
-      }
-      return e;
-    },
-    arithmetic: {
-      type: "modular",
-      modulus: 8,
-    },
-  },
-};
-
-// Automatically make it so that colorScheme.name is the key used to find the
-// colorScheme in the colorSchemes object.
-for (var colorSchemeName in colorSchemes) {
-  colorSchemes[colorSchemeName].name = colorSchemeName;
+  resquare(e) {
+    throw new Error("resquare method must be implemented by subclass");
+  }
 }
+
+class BWColorScheme extends ColorScheme {
+  constructor() {
+    super("BW", {
+      1: { fill: "white" },
+      2: { fill: "#333333" }
+    }, {
+      type: "modular",
+      modulus: 2
+    });
+  }
+
+  unsquare(e) {
+    return e == 1 ? 2 : e == 2 ? 1 : 0;
+  }
+
+  resquare(e) {
+    return e == 1 ? 2 : e == 2 ? 1 : 0;
+  }
+}
+
+class TriColorScheme extends ColorScheme {
+  constructor() {
+    super("tri", {
+      1: { fill: "#F9FAC8" },
+      2: { fill: "#0431D4" },
+      3: { fill: "#FF0040" }
+    }, {
+      type: "modular",
+      modulus: 3
+    });
+  }
+
+  unsquare(e) {
+    return e == 1 ? 3 : e == 3 ? 2 : e == 2 ? 1 : 0;
+  }
+
+  resquare(e) {
+    return e == 3 ? 1 : e == 2 ? 3 : e == 1 ? 2 : 0;
+  }
+}
+
+class RainbowColorScheme extends ColorScheme {
+  constructor() {
+    super("rainbow", {
+      1: { fill: "#BE81F7" },
+      2: { fill: "#5882FA" },
+      3: { fill: "#58FAF4" },
+      4: { fill: "#82FA58" },
+      5: { fill: "#F4FA58" },
+      6: { fill: "#FE9A2E" },
+      7: { fill: "#FA5858" }
+    }, {
+      type: "",
+      modulus: 7
+    });
+  }
+
+  unsquare(e) {
+    e++;
+    if (e == 8) {
+      e = 1;
+    }
+    return e;
+  }
+
+  resquare(e) {
+    e--;
+    if (e == 0) {
+      e = 7;
+    }
+    return e;
+  }
+}
+
+class Rainbow2ColorScheme extends ColorScheme {
+  constructor() {
+    super("rainbow2", {
+      1: { fill: "white" },
+      2: { fill: "#BE81F7" },
+      3: { fill: "#5882FA" },
+      4: { fill: "#58FAF4" },
+      5: { fill: "#82FA58" },
+      6: { fill: "#F4FA58" },
+      7: { fill: "#FE9A2E" },
+      8: { fill: "#FA5858" }
+    }, {
+      type: "modular",
+      modulus: 8
+    });
+  }
+
+  unsquare(e) {
+    e++;
+    if (e == 9) {
+      e = 1;
+    }
+    return e;
+  }
+
+  resquare(e) {
+    e--;
+    if (e == 0) {
+      e = 8;
+    }
+    return e;
+  }
+}
+
+// Create instances of color schemes
+const colorSchemes = {
+  BW: new BWColorScheme(),
+  tri: new TriColorScheme(),
+  rainbow: new RainbowColorScheme(),
+  rainbow2: new Rainbow2ColorScheme()
+};
 
 let colorSchemeByMod = {
   [2]: colorSchemes.BW,
   [3]: colorSchemes.tri,
-}
-
+};
 
 let modular_arithmetic_colors_cells = {
   [2]: colorSchemes.BW.cells,
   [3]: colorSchemes.tri.cells
-}
+};
 
 function get_arithmetic_color(n, arithmetic) {
   if (arithmetic.type == "modular") {
