9dcd5d2799ad55f424a6298fa630462236309ee7
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-08-24 00:22:47 -0400

    refactor: Convert Grid.js to modern ES6 class structure with inheritance
    - Converted pseudo-classical inheritance to modern ES6 classes
    - Created proper class hierarchy: Grid -> BoundedGrid -> GridFromArray/GridWindow
    - Moved static methods to static class methods
    - Used const instead of var for better scoping
    - Added proper constructor methods with super() calls
    - Maintained all existing functionality and inheritance relationships

--- web/www/Grid.js
+++ web/www/Grid.js
@@ -1,149 +1,155 @@
 /*
- * This file is written in a pseudo-classical object oriented manner.
+ * This file is written using modern ES6 classes with inheritance.
  */
 
-function Grid() {}
-//abstract function get(x,y);
-//abstract function set(x,y,v);
-
-function BoundedGrid() {}
-BoundedGrid.prototype = Object.create(Grid.prototype);
-//int width;
-//int height;
-
-BoundedGrid.prototype.forEach = function (f) {
-  //f(v,x,y,grid)
-  for (var i = 0; i < this.width; i++) {
-    for (var j = 0; j < this.height; j++) {
-      f(this.get(i, j), i, j, this);
-    }
+class Grid {
+  // Abstract base class - get and set methods should be implemented by subclasses
+  get(x, y) {
+    throw new Error("get method must be implemented by subclass");
   }
-};
-
-BoundedGrid.prototype.forEachSet = function (f) {
-  //f(v,x,y,grid)
-  for (var i = 0; i < this.width; i++) {
-    for (var j = 0; j < this.height; j++) {
-      this.set(i, j, f(this.get(i, j), i, j, this));
-    }
+  
+  set(x, y, v) {
+    throw new Error("set method must be implemented by subclass");
   }
-};
 
-BoundedGrid.prototype.to2dArray = function () {
-  let ret = [];
-  for (var j = 0; j < this.height; j++) {
-    let row = [];
-    ret.push(row);
-    for (var i = 0; i < this.width; i++) {
-      row.push(this.get(i, j));
-    }
+  window(x, y, w, h) {
+    return new GridWindow(this, x, y, w, h);
   }
-  return ret;
-};
 
-BoundedGrid.prototype.setAll = function (v) {
-  this.forEachSet(function () {
-    return v;
-  });
-};
+  static from2dArray(a) {
+    const h = a.length;
+    const w = a[0].length;
+    const flat = Array.prototype.concat.apply([], a);
+    return new GridFromArray(flat, w, h);
+  }
 
-function GridWindow(original, x, y, w, h) {
-  this.original = original;
-  this.x = x;
-  this.y = y;
-  this.width = w;
-  this.height = h;
+  static usingFlatArray(a, w, h) {
+    return new GridFromArray(a, w, h);
+  }
+
+  static empty(w, h) {
+    return new GridFromArray(new Array(w * h), w, h);
+  }
+
+  static withArrayConstructor = {
+    blank(con, w, h) {
+      return new GridFromArray(new con(w * h), w, h);
+    },
+    
+    fromArray(con, array) {
+      const h = array.length;
+      const w = array[0].length;
+      const grid = new GridFromArray(new con(w * h), w, h);
+      grid.forEach(function (_, x, y) {
+        grid.set(x, y, array[y][x]);
+      });
+      return grid;
+    },
+    
+    fromFunction(con, w, h, f) {
+      const grid = new GridFromArray(new con(w * h), w, h);
+      grid.forEach(function (_, x, y) {
+        grid.set(x, y, f(x, y));
+      });
+      return grid;
+    },
+  };
 }
 
-GridWindow.prototype = Object.create(BoundedGrid.prototype);
-
-GridWindow.prototype.get = function (x, y) {
-  return this.original.get(x + this.x, y + this.y);
-};
-
-GridWindow.prototype.set = function (x, y, v) {
-  return this.original.set(x + this.x, y + this.y, v);
-};
-
-Grid.prototype.window = function (x, y, w, h) {
-  return new GridWindow(this, x, y, w, h);
-};
-
-//Either cells of the original grid, otherwise access `virtual`
-function VirtualGrid(original, virtual) {
-  this.original = original;
-  this.virtual = virtual;
-}
-
-VirtualGrid.prototype = Object.create(Grid.prototype);
-
-VirtualGrid.prototype.get = function (x, y) {
-  if (x < 0 || y < 0 || x >= this.original.width || y >= this.original.height) {
-    return this.virtual(x, y, this.original);
-  } else {
-    return this.original.get(x, y);
+class BoundedGrid extends Grid {
+  forEach(f) {
+    // f(v,x,y,grid)
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        f(this.get(i, j), i, j, this);
+      }
+    }
   }
-};
 
-BoundedGrid.prototype.virtual = function (fn) {
-  return new VirtualGrid(this, fn);
-};
+  forEachSet(f) {
+    // f(v,x,y,grid)
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        this.set(i, j, f(this.get(i, j), i, j, this));
+      }
+    }
+  }
 
-function GridFromArray(a, w, h) {
-  this.array = a;
-  this.width = w;
-  this.height = h;
-}
+  to2dArray() {
+    const ret = [];
+    for (let j = 0; j < this.height; j++) {
+      const row = [];
+      ret.push(row);
+      for (let i = 0; i < this.width; i++) {
+        row.push(this.get(i, j));
+      }
+    }
+    return ret;
+  }
 
-GridFromArray.prototype = Object.create(BoundedGrid.prototype);
-
-GridFromArray.prototype.get = function (x, y) {
-  return this.array[this.width * y + x];
-};
-
-GridFromArray.prototype.set = function (x, y, v) {
-  return (this.array[this.width * y + x] = v);
-};
-
-GridFromArray.prototype.clone = function () {
-  return new GridFromArray(this.array.slice(), this.width, this.height);
-};
-
-Grid.from2dArray = function (a) {
-  var h = a.length;
-  var w = a[0].length;
-  var flat = Array.prototype.concat.apply([], a);
-  return new GridFromArray(flat, w, h);
-};
-
-Grid.usingFlatArray = function (a, w, h) {
-  return new GridFromArray(a, w, h);
-};
-
-Grid.empty = function (w, h) {
-  return new GridFromArray(new Array(w * h), w, h);
-};
-
-// The point is to be able to build a grid with different backing arrays
-// (normal array, or one of the "typed" arrays)
-Grid.withArrayConstructor = {
-  blank: function (con, w, h) {
-    return new GridFromArray(new con(w * h), w, h);
-  },
-  fromArray: function (con, array) {
-    var h = array.length;
-    var w = array[0].length;
-    var grid = new GridFromArray(new con(w * h), w, h);
-    grid.forEach(function (_, x, y) {
-      grid.set(x, y, array[y][x]);
+  setAll(v) {
+    this.forEachSet(function () {
+      return v;
     });
-    return grid;
-  },
-  fromFunction: function (con, w, h, f) {
-    var grid = new GridFromArray(new con(w * h), w, h);
-    grid.forEach(function (_, x, y) {
-      grid.set(x, y, f(x, y));
-    });
-    return grid;
-  },
-};
+  }
+
+  virtual(fn) {
+    return new VirtualGrid(this, fn);
+  }
+}
+
+class GridWindow extends BoundedGrid {
+  constructor(original, x, y, w, h) {
+    super();
+    this.original = original;
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+  }
+
+  get(x, y) {
+    return this.original.get(x + this.x, y + this.y);
+  }
+
+  set(x, y, v) {
+    return this.original.set(x + this.x, y + this.y, v);
+  }
+}
+
+class VirtualGrid extends Grid {
+  constructor(original, virtual) {
+    super();
+    this.original = original;
+    this.virtual = virtual;
+  }
+
+  get(x, y) {
+    if (x < 0 || y < 0 || x >= this.original.width || y >= this.original.height) {
+      return this.virtual(x, y, this.original);
+    } else {
+      return this.original.get(x, y);
+    }
+  }
+}
+
+class GridFromArray extends BoundedGrid {
+  constructor(a, w, h) {
+    super();
+    this.array = a;
+    this.width = w;
+    this.height = h;
+  }
+
+  get(x, y) {
+    return this.array[this.width * y + x];
+  }
+
+  set(x, y, v) {
+    return (this.array[this.width * y + x] = v);
+  }
+
+  clone() {
+    return new GridFromArray(this.array.slice(), this.width, this.height);
+  }
+}
