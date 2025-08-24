5bf6c861bcac8657c4c0a763443fd41695a794f3
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-08-24 00:14:30 -0400

    refactor: Convert api.js to modern ES6 class structure
    - Created Api class to encapsulate API functionality
    - Created Storage class to encapsulate localStorage operations
    - Replaced IIFE pattern with modern class constructors
    - Added JSDoc comments for better documentation
    - Used let instead of var for better scoping
    - Maintained backward compatibility with global api and storage instances

--- web/www/api.js
+++ web/www/api.js
@@ -1,42 +1,57 @@
 
-var api = (function () {
-  var onReady = [];
-
-  var api = {};
-
-  api.ready = function (fn) {
-    //onReady.push(fn);
-    fn();
-  };
-
-  function finished() {
-    for (var i = 0; i < onReady.length; i++) {
-      onReady[i]();
-    }
+class Api {
+  constructor() {
+    this.onReady = [];
   }
 
-  return api;
-})();
+  /**
+   * Executes a function when the API is ready
+   * @param {Function} fn - The function to execute
+   */
+  ready(fn) {
+    // this.onReady.push(fn);
+    fn();
+  }
 
+  /**
+   * Executes all ready callbacks
+   * @private
+   */
+  _finished() {
+    for (let i = 0; i < this.onReady.length; i++) {
+      this.onReady[i]();
+    }
+  }
+}
 
-
-var storage = (function () {
-  var storage = {};
-
-  storage.load = function (id, callback) {
+class Storage {
+  /**
+   * Loads data from localStorage
+   * @param {string} id - The key to load from
+   * @param {Function} callback - Callback function with loaded data
+   */
+  load(id, callback) {
     callback({
       data: localStorage.getItem(id),
     });
-  };
+  }
 
-  storage.save = function (id, data, callback) {
+  /**
+   * Saves data to localStorage
+   * @param {string} id - The key to save to
+   * @param {any} data - The data to save
+   * @param {Function} callback - Optional callback function
+   */
+  save(id, data, callback) {
     localStorage.setItem(id, data);
     callback &&
       callback({
         data: data,
       });
-  };
+  }
+}
 
-  return storage;
-})();
+// Create global instances for backward compatibility
+const api = new Api();
+const storage = new Storage();
 
