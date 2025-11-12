14c748ae4253d3ebe9a977d432386af90cf99bbe
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-11-12 00:20:45 -0500

    0.7.3 fix rage-quitting level

--- modules/ui/GameLevelMenu.js
+++ modules/ui/GameLevelMenu.js
@@ -6,7 +6,7 @@ import { book_reviver } from '../core/bookUtils.js';
 
 export class GameLevelMenu {
   constructor() {
-    this.bookUrl = "data/2025_nov_11_reordered_solved.json";
+    this.bookUrl = "data/2025_nov_11_reordered_solved_fixed.json";
     // this.bookUrl = "data/tiny_for_testing.json";
 
     // Delay the creation of level menu until after game is ready
