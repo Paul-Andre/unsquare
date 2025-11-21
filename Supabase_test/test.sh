7c62a2d7ea729ae4730cce8601e029ee87bbb72a
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-11-21 00:44:03 -0500

    trying to get Supabase to work...

---
+++ Supabase_test/test.sh
@@ -0,0 +1,4 @@
+curl -i --location --request POST 'https://vatpvuolfdnkcgdwgsxm.supabase.co/functions/v1/hello' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhdHB2dW9sZmRua2NnZHdnc3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MTc3OTMsImV4cCI6MjA3OTE5Mzc5M30.XEJsuWMrWzo1l2otg36z9uZ1Vm3BbItfnhb0r-Ne1NA' \
+  --header 'Content-Type: application/json' \
+  --data '{"name":"Functions"}'
