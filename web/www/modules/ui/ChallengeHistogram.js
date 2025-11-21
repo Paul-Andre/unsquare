ff9a43ac24486572e8e15ec2cc8c4cdebce6d517
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-11-21 18:39:57 -0500

    histogram tooltip now is directly over bar

--- web/www/modules/ui/ChallengeHistogram.js
+++ web/www/modules/ui/ChallengeHistogram.js
@@ -66,7 +66,7 @@ export function renderHistogram(container, histogramData, playerMoves) {
       tooltip.className = "histogramTooltip";
       const solutionText = count === 1 ? "solution" : "solutions";
       tooltip.textContent = `${moveCount} moves: ${count} ${solutionText}`;
-      barContainer.appendChild(tooltip);
+      bar.appendChild(tooltip);
 
       barContainer.addEventListener("mouseenter", () => {
         tooltip.classList.add("visible");
