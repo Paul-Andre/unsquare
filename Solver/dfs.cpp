d9f0d0577c4ce7144743e591e59e47c71a3ab751
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2025-12-01 18:29:43 -0500

    Working on dfs solver

--- Solver/dfs.cpp
+++ Solver/dfs.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int w,h;
 
 #ifndef N
-#define N 10
+#define N 11
 #endif
 
 using State = std::bitset<N * N>;
@@ -56,6 +56,7 @@ void print(const State &v, ostream& out=cerr){
 }
 
 vector<State> moves;
+vector<State> notCovered;
 
 vector<int> firstBitPosition;
 vector<int> gap;
@@ -66,7 +67,7 @@ int moves_target;
 bool found_solution = false;
 
 
-void solve(State &state, int move_ptr, int moves_done) {
+void solve1(State &state, int move_ptr, int moves_done) {
   if (moves_done > moves_target) return;
   int remainingOptions = moves.size() - move_ptr;
   int remainingTarget = moves_target-moves_done;
@@ -84,42 +85,90 @@ void solve(State &state, int move_ptr, int moves_done) {
     // Fixed move
     int fb = firstBitPosition[move_ptr];
     if (state[fb]) {
-      runningSolution.push_back(1);
       state ^= moves[move_ptr];
 
-      bool good = true;
-      for (int i = 1; i<gap[move_ptr]; i++) {
-        if (state[fb+i]) good = false;
+      /* bool good = true; */
+      /* for (int i = 1; i<gap[move_ptr]; i++) { */
+      /*   if (state[fb+i]) good = false; */
+      /* } */
+
+      bool good = (state & notCovered[move_ptr]) == 0;
+      if (good) {
+        runningSolution.push_back(1);
+        solve1(state, move_ptr+1, moves_done+1);
+        runningSolution.pop_back();
       }
-      if (good) solve(state, move_ptr+1, moves_done+1);
 
       state ^= moves[move_ptr];
-      runningSolution.pop_back();
     } else {
-      runningSolution.push_back(0);
 
-      bool good = true;
-      for (int i = 1; i<gap[move_ptr]; i++) {
-        if (state[fb+i]) good = false;
+      /* bool good = true; */
+      /* for (int i = 1; i<gap[move_ptr]; i++) { */
+      /*   if (state[fb+i]) good = false; */
+      /* } */
+      bool good = (state & notCovered[move_ptr]) == 0;
+      if (good) {
+        runningSolution.push_back(0);
+        solve1(state, move_ptr+1, moves_done);
+        runningSolution.pop_back();
       }
-      if (good) solve(state, move_ptr+1, moves_done);
 
-      runningSolution.pop_back();
+
     }
     return;
   }
 
     runningSolution.push_back(1);
     state ^= moves[move_ptr];
-    solve(state, move_ptr+1, moves_done+1);
+    solve1(state, move_ptr+1, moves_done+1);
     state ^= moves[move_ptr];
     runningSolution.pop_back();
 
     runningSolution.push_back(0);
-    solve(state, move_ptr+1, moves_done);
+    solve1(state, move_ptr+1, moves_done);
     runningSolution.pop_back();
 
 
+}
+
+void solve2(State &state, int move_ptr, int moves_done) {
+  if (moves_done > moves_target) return;
+  int remainingOptions = moves.size() - move_ptr;
+  int remainingTarget = moves_target-moves_done;
+  if (remainingOptions < remainingTarget) return;
+  if (move_ptr >= moves.size()) {
+      print(runningSolution, cout);
+      moves_target = min(moves_target, sumVec(runningSolution));
+      cerr<<sumVec(runningSolution)<<endl;
+      found_solution = true;
+    return;
+  }
+  if (gap[move_ptr]) {
+    state ^= moves[move_ptr];
+    if ((state & notCovered[move_ptr]) == 0) {
+      runningSolution.push_back(1);
+      solve2(state, move_ptr+1, moves_done+1);
+      runningSolution.pop_back();
+    }
+    state ^= moves[move_ptr];
+
+    if ((state & notCovered[move_ptr]) == 0) {
+      runningSolution.push_back(0);
+      solve2(state, move_ptr+1, moves_done);
+      runningSolution.pop_back();
+    }
+    return;
+  }
+
+    state ^= moves[move_ptr];
+    runningSolution.push_back(1);
+    solve2(state, move_ptr+1, moves_done+1);
+    runningSolution.pop_back();
+    state ^= moves[move_ptr];
+
+    runningSolution.push_back(0);
+    solve2(state, move_ptr+1, moves_done);
+    runningSolution.pop_back();
 }
 
 int main() {
@@ -184,16 +233,27 @@ int main() {
   }
   gap.back() = n*m - firstBitPosition.back();
 
+  notCovered = vector<State>(moves.size(), 0);
+  notCovered.back() = ~State(0);
+  State covered(0);
+
+  for (int i=moves.size()-1; i>=0; i--) {
+    notCovered[i] = ~covered;
+    covered |= moves[i];
+  }
+
+
   for (int i=0; i<moves.size(); i++) {
-    print(moves[i]);
-    cerr<<firstBitPosition[i]<<" ";
-    cerr<<gap[i]<<endl;
+    //print(moves[i]);
+    //print(notCovered[i]);
+    //cerr<<firstBitPosition[i]<<" ";
+    //cerr<<gap[i]<<endl;
   }
 
   State state = target;
   for (moves_target = 1; moves_target<20 && !found_solution; moves_target++) {
     cerr<<"trying with "<<moves_target<<" moves."<<endl;
-    solve(state, 0, 0);
+    solve1(state, 0, 0);
   }
   /* moves_target = 200; */
   /* solve(state, 0, 0); */
